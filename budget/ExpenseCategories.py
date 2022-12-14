524d020c7aa5975386b4699a940ab2317e6e5c2c
rpa20 <ryan_aboussafy@outlook.com>
2022-12-14 08:54:44 -0500

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,7 @@
+from budget.FrequentExpenses import expenses
 from . import Expense
 import matplotlib.pyplot as plt
+
 
 def main():
     expenses = Expense.Expenses()
@@ -9,3 +11,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+divided_set_comp = expenses.categorize_set_comprehension()
+
+if not divided_set_comp == divided_for_loop:
+    print('Sets are NOT equal by == test')
+
+for a, b in zip(divided_for_loop, divided_set_comp):
+    if not (a.issubset(b) and
+            b.issubset(a)):
+        print("Sets are NOT equal by subset test")
